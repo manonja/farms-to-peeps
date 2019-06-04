dac00308112bc4f9cb61bc8f3564d71edf085c64
Manon Jacquin <manon.jacquin@gmail.com>
2019-06-04 20:48:45 +0100

    Creates pages folder

---
+++ src/pages/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div className="navbar">
+                <a href="#" className="logo"></a>
+            </div>
+         );
+    }
+}
+ 
+export default Navbar;
