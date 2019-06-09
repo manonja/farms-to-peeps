30f9b73ec3f9f37437ee4038c42dba5424810432
Manon Jacquin <manon.jacquin@gmail.com>
2019-06-09 16:12:12 +0100

    remove product from basket works

--- src/customerComponents/Basket.js
+++ src/customerComponents/Basket.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 
+import BasketCard from './BasketCard'
+
 class Basket extends Component {
-    state = {  }
+    state = { 
+        email: this.props.email
+    }
     render() { 
-        return ( 
-            <div>I am the customer basket</div>
-         );
+        const {basket} = this.props
+        const product = basket.map(product => (
+            <BasketCard 
+                key={product.id}
+                product={product}
+                removeFromBasket={this.props.removeFromBasket}
+            />
+        ))
+        return (
+            <div className= 'productCollection'> {product} </div>
+        )
     }
 }
  
--- src/customerComponents/BasketCard.js
+++ src/customerComponents/BasketCard.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 
-class ProductCard extends Component {
+class BasketCard extends Component {
+
 
     render() {
         const {url_img, name, price, quantity} = this.props.product
@@ -12,11 +13,11 @@ class ProductCard extends Component {
                     <a><div className="mask rgba-white-slight"></div></a>
                 </div>
                 <div className="card-body card-body-cascade">
-                    <h5 className="pink-text pb-2 pt-1"><i className="fas fa-utensils"></i> Category</h5>
+                    <h5 className="pink-text pb-2 pt-1"><i className="fas fa-utensils"></i> Organic</h5>
                     <h4 className="font-weight-bold card-title">{name}</h4>
                     <p className="card-text">£{price}</p>
                     <p className="card-text">{quantity}</p>
-                    <button onClick={() => this.props.addToBasket(this.props.product)} className="btn btn-unique" >ADD TO BASKET</button>
+                    <button onClick={() => this.props.removeFromBasket(this.props.product)} className="btn btn-unique" >REMOVE FROM BASKET</button>
                 </div>
                 <div className="card-footer text-muted text-center">Chalk Farm</div>
             </div>
@@ -24,4 +25,4 @@ class ProductCard extends Component {
     }
 }
 
-export default ProductCard
+export default BasketCard
