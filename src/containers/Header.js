5bf7e05d58c5dbdcf25467df3506b7d42fa660fd
Manon Jacquin <manon.jacquin@gmail.com>
2019-06-16 20:03:30 +0100

    Fix navbar

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -21,49 +21,48 @@ class Header extends Component {
             </button>
     
             <div className="collapse navbar-collapse" id="navbar-header">
-                <ul className="navbar-nav mr-auto">
-                    <li>
+                <ul className="navbar-nav mr-auto shop">
                         <Link to='/products'><p className="shop">Shop</p></Link>
-                    </li>
+                        <Link to='/'><p className="shop">About</p></Link>
+
                 </ul>
-                    
-                   
-                <ul className='nav form-inline my-8 my-lg-9 ml-auto '>
+            
+                                         
+                <ul className='navbar-nav mx-auto'>
                 
-                <h5> 
                 {   
                     !this.props.current_user  
-                    ? 
-                        <Link to='/signup'><button className="btn btn-outline-dark btn-lg my-2 my-sm-0 ml-3">REGISTER</button></Link> 
-                    : `Welcome back, ${this.props.current_user.first_name}!`}
-                </h5>
-
-                <div>
+                    ?       
+                        <Link to='/signup'>REGISTER</Link> 
+                    : `Welcome (back), ${this.props.current_user.first_name}!`
+                }
+                </ul>
+                    
                     { noUserLoggedIn 
-                        ?  <div>
-                            <Link to='/signin'><li>Login</li></Link>
-                            </div>
+                        ?  
+                            <Link to='/signin'>Login</Link>
+                         
                         : (
                             isCustomer 
-                            ?   <div>
-                                    <Link to='/basket'><i className="fas fa-shopping-basket"></i></Link>
+                            ?   <ul className='navbar-nav ml-auto'>
                                     <Link to='/peep-profile'><p className="my-2 my-sm-0 ml-3">My profile</p></Link> 
+                                    <li></li><Link to='/basket'><i className="fas fa-shopping-basket"></i></Link>
                                     <button onClick={this.props.signout} className="btn btn-outline-orange btn-md my-2 my-sm-0 ml-3"          
                                     type="submit">Logout
                                     </button>
-                                
-                                </div>
-                            :  <div>
-                                    <Link to='/farmer-profile'><button className="btn btn-outline-dark btn-md my-2 my-sm-0 ml-3">My profile</button></Link> 
-                                    <button onClick={this.props.signout} className="btn btn-outline-green btn-md my-2 my-sm-0 ml-3"          type="submit">Logout
-                                    </button>
-                                </div>
+                                </ul>
+                            :  <ul className='navbar-nav ml-auto'>
+                                    <li>
+                                        <Link to='/farmer-profile'><button className="btn btn-outline-dark btn-md my-2 my-sm-0 ml-3">My profile</button></Link> 
+                                    </li>
+                                    <li> <button onClick={this.props.signout} className="btn btn-outline-green btn-md my-2 my-sm-0 ml-3"          type="submit">Logout
+                                        </button>
+                                    </li>
+                                </ul>
                         )
      
                     }
-                </div>       
 
-            </ul>
             </div>
             </nav>
          );
